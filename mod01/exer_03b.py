43358d04e94652a14918cd8bf45d642c2579d933
Emanuele Garieri <Emanuele.Garieri@metropolia.fi>
2026-08-30 09:20:59 +0300

    Python - Project Assignment 1

---
+++ mod01/exer_03b.py
@@ -0,0 +1,6 @@
+import math
+
+radius = float(input("Enter the radius: "))
+area = math.pi * radius ** 2
+
+print("Area:", area)
---
+++ mod01/exer_03d.py
@@ -0,0 +1,11 @@
+number1 = int(input("Enter first number: "))
+number2 = int(input("Enter second number: "))
+number3 = int(input("Enter third number: "))
+
+sum_numbers = number1 + number2 + number3
+product = number1 * number2 * number3
+average = sum_numbers / 3
+
+print("Sum:", sum_numbers)
+print("Product:", product)
+print("Average:", average)
